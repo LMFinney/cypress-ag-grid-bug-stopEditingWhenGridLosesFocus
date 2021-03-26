32c58b3d4f5d19eccddc453cb1cfc55f461253f5
Lance Finney <lance.finney@oasisdigital.com>
2021-03-26 11:07:36 -0500

    Add basic ag-grid

--- apps/bug-demo/src/app/app.component.ts
+++ apps/bug-demo/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ColDef } from '@ag-grid-community/core';
 
 @Component({
   selector: 'cypress-ag-grid-bug-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'bug-demo';
+export class AppComponent  {
+
+  columnDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model', editable: true },
+    { field: 'price', editable: true }
+  ];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
+
 }
--- apps/bug-demo/src/app/app.module.ts
+++ apps/bug-demo/src/app/app.module.ts
@@ -1,11 +1,18 @@
+import { AgGridModule } from '@ag-grid-community/angular';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ModuleRegistry } from '@ag-grid-community/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+]);
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AgGridModule.withComponents([])],
   providers: [],
   bootstrap: [AppComponent],
 })
